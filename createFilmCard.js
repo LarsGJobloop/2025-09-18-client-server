157174cdc99edb0aac25881aa8364b456d6d7e31
zabronax <zabronax@gmail.com>
2025-09-18 12:35:25 +0200

    add image to film card

--- createFilmCard.js
+++ createFilmCard.js
@@ -1,5 +1,15 @@
 export function createFilmCard(filmDetails) {
+  // Create elements
   const card = document.createElement("li")
-  card.textContent = filmDetails.title
+  const titleElement = document.createElement("p")
+  const imageElement = document.createElement("img")
+
+  titleElement.textContent = filmDetails.title
+  imageElement.src = filmDetails.image
+
+  // Hook up/connect them together
+  card.append(titleElement)
+  card.append(imageElement)
+
   return card
 }
