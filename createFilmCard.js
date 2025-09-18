cfeac281c5aaa829362cd74268e72b6170cb19ca
zabronax <zabronax@gmail.com>
2025-09-18 12:48:19 +0200

    add metadata to film cards

--- createFilmCard.js
+++ createFilmCard.js
@@ -4,6 +4,14 @@ export function createFilmCard(filmDetails) {
     <li>
       <p>${filmDetails.title}</p>
       <img src="${filmDetails.image}" alt="" />
+
+      <footer>
+        <p>${filmDetails.director}</p>
+        <p>${filmDetails.producer}</p>
+        <p>${filmDetails.release_date}</p>
+        <p>${filmDetails.running_time}</p>
+        <p>${filmDetails.rt_score}</p>
+      </footer>
     </li>
   `
 
