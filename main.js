967bdeb1d56f6a0ea7192163b39f25988196dbc3
zabronax <zabronax@gmail.com>
2025-09-18 11:27:16 +0200

    use local json files instead of remote APIs

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ console.log("JavaScript loaded!")
 
 // The addres we want to requst data from
 // Open it in your browser
-const url = "https://ghibliapi.vercel.app/films/"
+// const url = "https://ghibliapi.vercel.app/films/"
+// We can fetch local files
+// const url = "http://127.0.0.1:5500/films.json"
+// If the HTML and files are from the same IP
+// we can skip the address and just use the path
+const url = "/films.json"
 // Send requst and await response
 const response = await fetch(url)
 console.log(response)
